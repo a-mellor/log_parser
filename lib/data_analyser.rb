b12e82e53e904f18ff3adcfc802f31489b3ed0ac
a-mellor <alex.mellor@honeycomb.tv>
2020-03-30 00:18:37 +0100

    Add file extension validation
    Basic validation to only accept .log files.

---
+++ lib/data_analyser.rb
@@ -0,0 +1,21 @@
+class DataAnalyser
+  ACCEPTED_FILES = ['.log'].freeze
+
+  class InvalidFileError < StandardError; end
+
+  attr_reader :file
+
+  def initialize(file)
+    @file = file
+  end
+
+  def valid?
+    file_extension = File.extname(file)
+    unless ACCEPTED_FILES.include?(file_extension)
+      raise InvalidFileError,
+            "#{file_extension} is not vaild, please prodive #{ACCEPTED_FILES.join(',')} file"
+    end
+
+    true
+  end
+end
