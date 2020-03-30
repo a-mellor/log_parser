402041e2ea8ad2250a46e12b0bc4273b82005fab
a-mellor <alex.mellor@honeycomb.tv>
2020-03-30 19:13:29 +0100

    Add LogParser
    Responsible for returning data grouped by page and all the IP
    addresses that have visted that page.

---
+++ lib/log_parser.rb
@@ -0,0 +1,24 @@
+class LogParser
+  class NoDataError < StandardError; end
+
+  attr_reader :file
+
+  def initialize(file)
+    @file = file
+  end
+
+  def execute
+    raise NoDataError, 'The file provided is empty' if File.empty?(file)
+
+    data = Hash.new{|h,k| h[k] = [] }
+
+    f = File.open(file, "r")
+
+    f.each_line do |line|
+      page, ip_address = line.split(" ")
+      data[page].push ip_address 
+    end
+
+    data
+  end
+end
